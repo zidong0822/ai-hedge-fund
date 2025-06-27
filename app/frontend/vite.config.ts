ada26f1a8cb87f3f26977e34dbd77c717b566885
hongwei.he <zidong0822@sina.cn>
2025-06-27 11:51:18 +0800

    fix: docker配置

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,12 +1,12 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 允许外部访问
+    host: "0.0.0.0", // 允许外部访问
     port: 5173,
     watch: {
       usePolling: true, // Docker环境下需要
@@ -17,4 +17,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
